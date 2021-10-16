17c56552f7eb46229582799c8934f51c981c124d
Nathan Nelson <nathan.nelson042@gmail.com>
2021-10-16 00:47:58 -0700

    added an event to stop the broswer from refreshing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");      //Create a new task item
     listItemEl.className = "task-item";                 //Style the new task item
     listItemEl.textContent = "This is a new task.";     //Add the text
     tasksToDoEl.appendChild(listItemEl);                //Append this element to the task list
 }
 
-buttonEl.addEventListener("click", createTaskHandler);  //needs to go after the createTaskHandler function or it breaks the code.
+formEl.addEventListener("submit", createTaskHandler);  //needs to go after the createTaskHandler function or it breaks the code.

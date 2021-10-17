6190767c895687f6c1f462ea5604a23594c3905a
Nathan Nelson <nathan.nelson042@gmail.com>
2021-10-16 23:12:16 -0700

    refactored the code. divided it into two functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,27 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
 
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;  // by adding .value----Now the value of the taskNameInput variable will be the text we entered in the <input> element.
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+
+    // Package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+
+
+var createTaskEl = function (taskDataObj) {
+
     // Create list item
     var listItemEl = document.createElement("li");      //Create a new task item
     listItemEl.className = "task-item";                 //Style the new task item
@@ -18,11 +32,11 @@ var createTaskHandler = function (event) {
     taskInfoEl.className = "task-info";  // Give it a class name
 
     // Add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);               
+    tasksToDoEl.appendChild(listItemEl);
 }
 
-formEl.addEventListener("submit", createTaskHandler);  //needs to go after the createTaskHandler function or it breaks the code.
+formEl.addEventListener("submit", taskFormHandler);  //needs to go after the taskFormHandler function or it breaks the code.
